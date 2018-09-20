71d71a7cb633bea82678d17db352b0a448cebc45
Antonio Jesús Navarro Expósito <Antonio Jesus Navarro Exposito>
2018-09-20 11:23:17 +0200

    sass, bem y estilo hechos

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 var board = new Board();
 var paddle1Top;
 var paddle2Top;
-var paddleHeigth = parseInt($('.paddle').css('height'));
-var paddleWidth = parseInt($('.paddle').css('width'));
-var boardTop = parseInt($('#board').css('top'));
-var boardLeft = parseInt($("#board").css('left'));
-var boardHeight = parseInt($("#board").css("height"));
+var paddleHeigth = parseInt($('.board__paddle').css('height'));
+var paddleWidth = parseInt($('.board__paddle').css('width'));
+var boardTop = parseInt($('.board').css('top'));
+var boardLeft = parseInt($(".board").css('left'));
+var boardHeight = parseInt($(".board").css("height"));
 var intervalTime = 50;
 var winner;
 var game;
 
-$('#start').on('click', function() {
+$('.start').on('click', function() {
   board.start();
   var game = setInterval(updateState, intervalTime);
   renderGame();
@@ -25,24 +25,24 @@ function updateState() {
 
 $(document).on('keydown', function(e) {
   if (e.keyCode == 40) { //Arrow down
-    paddle2Top = parseInt($('#paddle2').css('top'));
+    paddle2Top = parseInt($('.board__paddle2').css('top'));
     if (paddleHeigth + paddle2Top < boardHeight)
     board.paddle2.moveDown(paddle2Top);
   }
   if (e.keyCode == 38) { //Arrow up
-    paddle2Top = parseInt($('#paddle2').css('top'));
+    paddle2Top = parseInt($('.board__paddle2').css('top'));
     if (paddle2Top > 0)
       board.paddle2.moveUp(paddle2Top);
   }
 
   if (e.keyCode == 65) { //A -> paddle1 down
-    paddle1Top = parseInt($('#paddle1').css('top'))
+    paddle1Top = parseInt($('.board__paddle1').css('top'))
     if (paddleHeigth + paddle1Top < boardHeight)
       board.paddle1.moveDown(paddle1Top)
   }
 
   if (e.keyCode == 81) { //Q -> paddle1 up
-    paddle1Top = parseInt($('#paddle1').css('top'));
+    paddle1Top = parseInt($('.board__paddle1').css('top'));
     if (paddle1Top > 0)
       board.paddle1.moveUp(paddle1Top);
   }
@@ -51,30 +51,30 @@ $(document).on('keydown', function(e) {
 function renderGame() {
   winner = board.ball.winner()
   if (winner == 1) {
-    $('#score1').children('span').text('win')
+    $('.leyend__score1').children('span').text('win')
     clearInterval(game)
   }
   if (winner == 2) {
-    $('#score2').children('span').text('win')
+    $('.leyend__score2').children('span').text('win')
     clearInterval(game)
   }
 }
 
 function renderScore(){
-  $('#score1').children('span').text(board.paddle1.score);
-  $('#score2').children('span').text(board.paddle2.score);
+  $('.leyend__score1').children('span').text(board.paddle1.score);
+  $('.leyend__score2').children('span').text(board.paddle2.score);
 }
 
 function renderBall(){
   board.ball.move()
-  $('#ball').css('top', board.ball.y + 'px')
-  $('#ball').css('left', board.ball.x + 'px')
+  $('.board__ball').css('top', board.ball.y + 'px')
+  $('.board__ball').css('left', board.ball.x + 'px')
  }
 
  function renderPaddle(){
-  $('#paddle1').css('top', board.paddle1.y + 'px')
-  $('#paddle1').css('left', board.paddle1.x + 'px')
+  $('.board__paddle1').css('top', board.paddle1.y + 'px')
+  $('.board__paddle1').css('left', board.paddle1.x + 'px')
 
-  $('#paddle2').css('top', board.paddle2.y + 'px')
-  $('#paddle2').css('left', board.paddle2.x + 'px')
+  $('.board__paddle2').css('top', board.paddle2.y + 'px')
+  $('.board__paddle2').css('left', board.paddle2.x + 'px')
  }
